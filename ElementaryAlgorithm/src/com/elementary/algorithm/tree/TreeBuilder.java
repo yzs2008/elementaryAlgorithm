69646541fceacc49ab54d17e8844f36b291ffa76
yzs <yzs2008@yeah.net>
2015-03-30 17:16:32 +0800

    builde tree with child and sibling reference. while the input expression is so called generalized listed;eg.R(A(D,E),B,C(F(G,H,K)))#

--- ElementaryAlgorithm/src/com/elementary/algorithm/tree/TreeBuilder.java
+++ ElementaryAlgorithm/src/com/elementary/algorithm/tree/TreeBuilder.java
@@ -2,6 +2,8 @@ package com.elementary.algorithm.tree;
 
 import java.util.Stack;
 
+import javax.swing.plaf.metal.MetalIconFactory.TreeFolderIcon;
+
 /**
  * @author yzs 
  * 根据树的广义表表示方法输入字符串，构建树结构
@@ -9,7 +11,7 @@ import java.util.Stack;
 public class TreeBuilder {
 
 	//expression 表达式以#结尾 
-	public TreeWithChildrenParent<Character> builder(String expression){
+	public TreeWithChildrenParent<Character> buildChildrenParentTree(String expression){
 		//节点栈
 		Stack<TreeWithChildrenParent<Character>> nodeStack =new Stack<TreeWithChildrenParent<Character>>();
 		//操作符栈
@@ -40,9 +42,56 @@ public class TreeBuilder {
 		return nodeStack.pop();
 	}
 	
+	//expression must end with '#'
+	public TreeSiblingChild<Character> builderSiblingChildTree(String expression){
+		int index=0;
+		char curInput=expression.charAt(index);
+		TreeSiblingChild<Character> root;
+		Stack<Character> opStack=new Stack<Character>();
+		Stack<TreeSiblingChild<Character>> nodeStack=new Stack<TreeSiblingChild<Character>>();
+		while(curInput!='#'){
+			switch(curInput){
+				case '(':
+					opStack.push(curInput);
+					break;
+				case ')':
+					char topOperator;
+					do{
+						topOperator=opStack.pop();
+						nodeStack.pop();
+					}while(topOperator!='(');
+					break;
+				case ',':
+					opStack.push(curInput);
+					break;
+				default:
+					TreeSiblingChild<Character> node=new TreeSiblingChild<Character>(curInput);
+					if(!opStack.isEmpty() && opStack.peek()=='('){//the first child
+						TreeSiblingChild<Character> parent=nodeStack.pop();
+						parent.firstChild=node;
+						nodeStack.push(parent);
+						nodeStack.push(node);
+					}else if(!opStack.isEmpty() && opStack.peek()==','){
+						TreeSiblingChild<Character> sibling=nodeStack.pop();
+						sibling.closedSibling=node;
+						nodeStack.push(sibling);
+						nodeStack.push(node);
+					}	
+					else{
+						nodeStack.push(node);
+					}
+					break;
+			}
+			curInput=expression.charAt(++index);
+		}
+		root=nodeStack.pop();
+		return root;
+	}
 	public static void main(String[] args) {
 		TreeBuilder builder=new TreeBuilder();
-		TreeWithChildrenParent<Character> root= builder.builder("R(A(D,E),B,C(F(G,H,K)))#");
+		String expression="R(A(D,E),B,C(F(G,H,K)))#";
+		TreeWithChildrenParent<Character> root= builder.buildChildrenParentTree(expression);
+		TreeSiblingChild<Character> root2=builder.builderSiblingChildTree(expression);
 
 	}
 
