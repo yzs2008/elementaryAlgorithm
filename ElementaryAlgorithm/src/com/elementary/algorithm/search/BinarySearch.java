9db79f978e92e7b9f8892112029ce856668a8ef3
yzs <yzs2008@yeah.net>
2015-04-02 21:27:09 +0800

    binary search.

---
+++ ElementaryAlgorithm/src/com/elementary/algorithm/search/BinarySearch.java
@@ -0,0 +1,15 @@
+package com.elementary.algorithm.search;
+
+/**
+ * binary search
+ * @author kaidi
+ *
+ */
+public class BinarySearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
