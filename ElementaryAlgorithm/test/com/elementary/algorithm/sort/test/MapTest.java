891f0485873c5fc14944dbeab72e8df0eb892e12
yzs <yzs2008@yeah.net>
2015-07-24 16:08:10 +0800

    测试文件，二分法搜索

--- ElementaryAlgorithm/test/com/elementary/algorithm/sort/test/MapTest.java
+++ ElementaryAlgorithm/test/com/elementary/algorithm/sort/test/MapTest.java
@@ -1,5 +1,13 @@
 package com.elementary.algorithm.sort.test;
 
+import java.util.Observable;
+
 public class MapTest {
 
+	Observable observable=new Observable();
+	
+	public void Change(){
+		//there is no way to call observable.setChange();
+	}
+	
 }
